a8aa6d3348caaad84018a7eda59265608f2dab83
speedfire <pashaprofy@i.ua>
2014-10-12 23:49:11 +0300

    Lastest work

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author pasha
+ */
+public class JavaApplication1 {
+    /**
+     * @param args the command line arguments
+     */
+     
+    
+    public static void main(String[] args) {
+        Frame m = new Frame();
+        m.setVisible(true);
+     
+    }
+    
+}
